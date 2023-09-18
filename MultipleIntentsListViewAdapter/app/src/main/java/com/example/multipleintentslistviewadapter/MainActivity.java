4d5cd85f09d152f1ac4c122dedd47654abc32184
SuperFire-92 <nickdyk21@gmail.com>
2023-09-18 15:10:39 -0400

    Finished
    Added a listview which can update to display new registered people in the list (dummy values placed to avoid resizing the array)

--- MultipleIntentsListViewAdapter/app/src/main/java/com/example/multipleintentslistviewadapter/MainActivity.java
+++ MultipleIntentsListViewAdapter/app/src/main/java/com/example/multipleintentslistviewadapter/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -18,12 +20,17 @@ public class MainActivity extends AppCompatActivity
     Intent int_j_registerIntent;
     //Looking at class variables and what happens when we return to this class from register.java
     static int x = 5;
+    static String[] registeredUsers = new String[3];
+    static int cnt = 0;
+    ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.d("x",x + "");
         btn_j_gotoRegister = findViewById(R.id.btn_v_gotoRegister);
         tv_j_welcome = findViewById(R.id.tv_v_welcome);
@@ -31,7 +38,34 @@ public class MainActivity extends AppCompatActivity
         //The intent you came from and the intent you want to load.
         int_j_registerIntent = new Intent(MainActivity.this, Register.class);
 
+        registeredUsers[0] = "Bob Smith";
+        registeredUsers[1] = "Bob Smithagin";
+        registeredUsers[2] = "Bob Smithanigan";
+        fillListView();
+
+        //try to see if there is a bundle being passed to MainActivity.java
+        //If there is, parse the data
+        //If there is not skip this code
+
+        //catch the info the was passed back to me by register
+        Intent cameFrom = getIntent();
+        //get the bundle that was passed.
+        Bundle infoPassedToMe = cameFrom.getExtras();
+        //Make sure we only do this if the activity is loading from Register.java
+        //The first time this loads we do not want to do this.
+        if (infoPassedToMe != null)
+        {
+            //get information in the bundle called "Name" set in Register.java
+            String fullName = infoPassedToMe.getString("Name");
+            //Log.d("Register Name",fullName);
+            registeredUsers[cnt] = fullName;
+            cnt++;
+            displayRegisteredUsers();
+            fillListView();
+        }
+
         registerButtonEventHandler();
+
     }
 
     public void registerButtonEventHandler()
@@ -48,5 +82,22 @@ public class MainActivity extends AppCompatActivity
                 startActivity(int_j_registerIntent);
             }
         });
+    }
+
+    public void displayRegisteredUsers()
+    {
+        for(int i = 0; i < cnt; i++)
+        {
+            Log.d("Registered User: ", registeredUsers[i]);
+        }
+    }
+
+    public void fillListView()
+    {
+        //Built in listviews and adapters only work with string arrays
+        //Changing any details of any specific cell will require it's own xml file.
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,registeredUsers);
+        //set the listViews adapter
+        lv_j_users.setAdapter(adapter);
     }
 }
